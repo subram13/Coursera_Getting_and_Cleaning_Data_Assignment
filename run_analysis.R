719a2b952707ff7b1ccece3f383dc27c6a40dabc
subram13 <subashini.mani@gmail.com>
2018-07-11 11:41:51 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,13 @@
+# Getting and Cleaning Data Project John Hopkins Coursera
+# Author: Subashini Subramanian
+
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names.
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+# Load Packages and get the Data
 library(reshape2)
 
 filename <- "getdata_dataset.zip"
