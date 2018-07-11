c62d781c1003423f923406c881a16d907cd7e01e
subram13 <subashini.mani@gmail.com>
2018-07-11 11:47:24 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -57,4 +57,4 @@ allData$subject <- as.factor(allData$subject)
 allData.melted <- melt(allData, id = c("subject", "activity"))
 allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
 
-write.table(allData.mean, "clean_data.txt", row.names = FALSE, quote = FALSE)
+write.table(allData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
